2e3a44a71c57ef48c00e5663507af92ba58d7bae
amemonkey <ameliafunaki789@gmail.com>
2018-06-08 15:19:21 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,11 +1,7 @@
 # Challenge One
 def usd_to_eur(dollar_amount)
-  if dollar_amount == 1.18
-    puts 1
-  else
-    puts "nil"
-  end
-  
+  if dollar_amount ==
+    
 
 def eur_to_usd(euro_amount)
   if euro_amount == 1
@@ -14,6 +10,22 @@ def eur_to_usd(euro_amount)
     puts "nil"
   end
   
-  usd_to_eur(3)
-  eur_to_usd(.75)
+  usd_to_eur(1.18)
+  eur_to_usd(1)
+  
+  
+  
+#   def fizzbuzz(int)
+# if int % 3==0; int % 5==0
+#   puts "FizzBuzz"
+#   elsif int % 3==0
+#   puts "Fizz"
+#   elsif int % 5==0
+#   puts "Buzz"
+# else
+#   puts "nil"
+# end
+# end
+
+# fizzbuzz(30)
     
