8a1fef18805d311ce24f360aa3ed70bccb60262e
amemonkey <ameliafunaki789@gmail.com>
2018-06-08 15:00:49 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,19 @@
-# Write your cod here
+# Challenge One
+def usd_to_eur(dollar_amount)
+  if dollar_amount == 1.18
+    puts 1
+  else
+    puts "nil"
+  end
+  
+
+def eur_to_usd(euro_amount)
+  if euro_amount == 1
+    puts 1.18
+  else
+    puts "nil"
+  end
+  
+  usd_to_eur(3)
+  eur_to_usd(.75)
+    
